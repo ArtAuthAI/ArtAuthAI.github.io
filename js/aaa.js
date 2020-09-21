3dc8ccc3e4060c86ccafd0e0e0d6d57e6f583609
Leon Kuhne <lxk1170@rit.edu>
2020-09-20 20:07:11 -0400

    added domain model & methodologies

--- js/aaa.js
+++ js/aaa.js
@@ -1,7 +1,9 @@
 const URL = {
   timesheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSvznfmljkFXaaKuFz0yBj2-KF8qndFx4aOfgvHYQA5Ikr_SkvDRMXd4yXWV4qTCAWInMoxAe_1bDV1/pubhtml?gid=890696192&amp;single=true&amp;widget=true&amp;headers=false",
   synopsis: "https://docs.google.com/document/d/e/2PACX-1vQkcWE1VPVjhiLXYHRVyJb6MEe1k15PtYHfp-f-wkHzPPJha75gOuquKa4RPHRwtnztoOquAB63QzQ6/pub?embedded=true",
-  skills: "https://docs.google.com/document/d/e/2PACX-1vRdCfse-zdoXIffZ2E4TqOcwawTUB7BiBwxOG2tQ6wIubLGou_G63iXfjS20APrmT1iwMgqBcw3KDqj/pub?embedded=true"
+  skills: "https://docs.google.com/document/d/e/2PACX-1vRdCfse-zdoXIffZ2E4TqOcwawTUB7BiBwxOG2tQ6wIubLGou_G63iXfjS20APrmT1iwMgqBcw3KDqj/pub?embedded=true",
+  plan: "https://docs.google.com/document/d/e/2PACX-1vQV72Mi6weDvFDCAliDXMHqGHHdJUt3dFebrmJvsPeD4EaOZQGfc3UU0W_eunE66Evb7kJ-jBbZVDg4/pub?embedded=true",
+  domain: "https://app.lucidchart.com/documents/embeddedchart/7246c3b9-2ed7-4202-9047-72c822a9c082",
 }
 
 updateNav = (pageName) => {
